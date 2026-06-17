ced1c512262ed7cb2ae1a48628719f96b114800a
Luis Felipe de Oliveira Brasil <felipebrasil18102005@gmail.com>
2026-06-16 21:31:39 -0300

    arrumando

--- cadastro/Cliente.java
+++ cadastro/Cliente.java
@@ -1,66 +1,69 @@
 package cadastro;
 
-public class Cliente<T, U> {
+public class Cliente<T, N> {
 
+    
     private T nome;
     private T sobrenome;
-    private U idade;
+    private N idade;
     private T endereco;
     private T email;
 
-    public Cliente(T nome, T sobrenome, U idade, T endereco, T email) {
-        this.nome = nome;
+    
+    public Cliente(T nome, T sobrenome, N idade, T endereco, T email) {
+        this.nome      = nome;
         this.sobrenome = sobrenome;
-        this.idade = idade;
-        this.endereco = endereco;
-        this.email = email;
+        this.idade     = idade;
+        this.endereco  = endereco;
+        this.email     = email;
     }
 
-    public T getNome() {
-        return nome;
-    }
+    
+    public T getNome()           
+    
+    { return nome; }
+    
+    public void setNome(T nome)  
+    
+    { this.nome = nome; }
 
-    public void setNome(T nome) {
-        this.nome = nome;
-    }
+    public T getSobrenome()                  
+    
+    { return sobrenome; }
+    
+    public void setSobrenome(T sobrenome)    
+    
+    { this.sobrenome = sobrenome; }
 
-    public T getSobrenome() {
-        return sobrenome;
-    }
+    public N getIdade()              
+    
+    { return idade; }
+    
+    public void setIdade(N idade)    
+    
+    { this.idade = idade; }
 
-    public T setSobrenome(T sobrenome) {
-        this.sobrenome = sobrenome;
-    }
+    public T getEndereco()                
+    
+    { return endereco; }
+    
+    public void setEndereco(T endereco)   
+    
+    { this.endereco = endereco; }
 
-    public U getIdade() {
-        return idade;
-    }
+    public T getEmail()              
+    
+    { return email; }
+    
+    public void setEmail(T email)    
+    
+    { this.email = email; }
 
-    public U setIdade(U idade) {
-        this.idade = idade;
-    }
-
-    public T getEndereco() {
-        return endereco;
-    }
-
-    public T setEndereco(T endereco) {
-        this.endereco = endereco;
-    }
-
-    public T getEmail() {
-        return email;
-    }
-
-    public T setEmail(T email) {
-        this.email = email;
-    }
-
-    public void imprimirDados() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Sobrenome: " + sobrenome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Endereço: " + endereco);
-        System.out.println("Email: " + email);
+    
+    public void imprimirCliente() {
+        System.out.println("Nome      : " + nome + " " + sobrenome);
+        System.out.println("Idade     : " + idade + " anos");
+        System.out.println("Endereço  : " + endereco);
+        System.out.println("E-mail    : " + email);
     }
 }
--- cadastro/ControleCliente.java
+++ cadastro/ControleCliente.java
@@ -4,14 +4,21 @@ public class ControleCliente {
 
     public static void main(String[] args) {
 
-        Cliente<String, Integer> cliente = new Cliente<>();
+       
+        Cliente<String, Integer> cliente = new Cliente<>(
+            "Felipe",
+            "Brasil",
+            21,
+            "Rua Luiz Carlos Ribeiro, Casa 207",
+            "felipe.brasil@email.com"
+        );
 
-        cliente.setNome("Felipe");
-        cliente.setSobrenome("Brasil");
-        cliente.setIdade(21);
-        cliente.setEndereco("Rua Luiz Carlos Ribeiro, Casa 207");
-        cliente.setEmail("felipe@gmail.com");
+        
+        cliente.imprimirCliente();
 
-        cliente.imprimirDados();
+        
+        System.out.println("\n>> Atualizando e-mail...\n");
+        cliente.setEmail("felipe.brasil@email.com");
+        cliente.imprimirCliente();
     }
 }
